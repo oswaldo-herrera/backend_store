f02cec5c6e5268acc30a54fc42ba2353e30dcc37
oswaldo-herrera <oswaldo.herrera.mx@gmail.com>
2025-11-05 00:09:44 -0600

    server.tsx

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,17 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  // 🛑 LÍNEAS CLAVE: Habilitar la confianza en el proxy (TRUST_PROXY)
+  url: env('PUBLIC_URL', 'https://backend-store-15n4.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
+    // Debes indicar a Strapi que confíe en los encabezados X-Forwarded-* (que usa Render)
+    proxy: true, 
+    // Aseguramos que Strapi considere la conexión HTTPS
+    secure: true,
+  },
+  // Opcional, pero a veces necesario:
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
