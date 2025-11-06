153667b88646a61da3e5f3d615a8027cbbd88f9a
oswaldo-herrera <oswaldo.herrera.mx@gmail.com>
2025-11-05 20:31:46 -0600

    proxy_2

--- config/server.ts
+++ config/server.ts
@@ -3,9 +3,9 @@ export default ({ env }) => ({
   port: env.int('PORT', 1337),
   // 🛑 LÍNEAS CLAVE: Habilitar la confianza en el proxy (TRUST_PROXY)
   url: env('PUBLIC_URL'),
-  proxy: true, 
   app: {
     keys: env.array('APP_KEYS'),
+    proxy: true, 
     // Debes indicar a Strapi que confíe en los encabezados X-Forwarded-* (que usa Render)
     // Aseguramos que Strapi considere la conexión HTTPS
     //secure: true,
