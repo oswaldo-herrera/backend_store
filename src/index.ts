1b4fe7f66536823624ba6988cc7538ce377dea8a
oswaldo-herrera <oswaldo.herrera.mx@gmail.com>
2025-11-20 15:08:27 -0600

    jorge change

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,15 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register({ strapi }) {
+    // comprobación de la librería 'cookies' que ignora la configuración de proxy de Koa.
+    strapi.server.use(async (ctx, next) => {
+      if (ctx.req?.socket) {
+        (ctx.req.socket as any).encrypted = true;
+      }
+      await next();
+    });
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -18,3 +26,27 @@ export default {
    */
   bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
 };
+
+
+
+
+// // import type { Core } from '@strapi/strapi';
+
+// export default {
+//   /**
+//    * An asynchronous register function that runs before
+//    * your application is initialized.
+//    *
+//    * This gives you an opportunity to extend code.
+//    */
+//   register(/* { strapi }: { strapi: Core.Strapi } */) {},
+
+//   /**
+//    * An asynchronous bootstrap function that runs before
+//    * your application gets started.
+//    *
+//    * This gives you an opportunity to set up your data model,
+//    * run jobs, or perform some special logic.
+//    */
+//   bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+// };
